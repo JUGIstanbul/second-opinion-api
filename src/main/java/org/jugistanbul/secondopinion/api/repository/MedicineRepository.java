f6cd8b4bea2adc0da4bfbff1dcb8db5ce8a1ceca
ajitatif <gokalp.gurbuzer@yandex.com>
2017-11-25 23:47:32 +0300

    Updated repositories to extend JpaRepository, rather than CRUDRepository

--- src/main/java/org/jugistanbul/secondopinion/api/repository/MedicineRepository.java
+++ src/main/java/org/jugistanbul/secondopinion/api/repository/MedicineRepository.java
@@ -1,12 +1,12 @@
 package org.jugistanbul.secondopinion.api.repository;
 
 import org.jugistanbul.secondopinion.api.entity.Medicine;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author Gökalp Gürbüzer (gokalp.gurbuzer@yandex.com)
  */
 @Repository
-public interface MedicineRepository extends CrudRepository<Medicine, Long> {
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
 }
