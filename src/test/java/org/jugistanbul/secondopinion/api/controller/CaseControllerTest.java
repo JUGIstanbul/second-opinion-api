0536206c82b7eef3ff0b124ff9908989b1c0596d
taner <taner.diler@gmail.com>
2017-11-26 00:04:09 +0300

    Save case to database

--- src/test/java/org/jugistanbul/secondopinion/api/controller/CaseControllerTest.java
+++ src/test/java/org/jugistanbul/secondopinion/api/controller/CaseControllerTest.java
@@ -2,6 +2,7 @@ package org.jugistanbul.secondopinion.api.controller;
 
 import org.jugistanbul.secondopinion.api.config.BaseIT;
 import org.jugistanbul.secondopinion.api.entity.Case;
+import org.jugistanbul.secondopinion.api.repository.CaseRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CaseControllerTest extends BaseIT {
@@ -20,19 +23,23 @@ public class CaseControllerTest extends BaseIT {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    CaseRepository caseRepository;
+
 
     @Test
     public void should_save_case() throws Exception {
         //given
         Case caseEntity = new Case();
-        caseEntity.setNote("Lorem ipsum dolor sit amet.");
-
 
         //when
         ResponseEntity entity = testRestTemplate.withBasicAuth("1","1").postForEntity("/v1/cases", caseEntity, ResponseEntity.class);
 
+        List<Case> caseList = caseRepository.findAll();
+
         //then
-        assertThat(entity).isNotNull();
+        assertThat(caseList.size()).isEqualTo(1);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(entity.getHeaders().get(HttpHeaders.LOCATION).get(0)).isEqualTo("/api/v1/cases/1");
     }
 }
