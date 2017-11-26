64096ab81537f1e91571aa4873a37841ad819646
Erdem Ilhan <eilhan@deloitte.com>
2017-11-26 13:12:51 +0300

    patient repository and converters added with updated tests

--- src/test/java/org/jugistanbul/secondopinion/api/service/PatientServiceTest.java
+++ src/test/java/org/jugistanbul/secondopinion/api/service/PatientServiceTest.java
@@ -1,17 +1,23 @@
 package org.jugistanbul.secondopinion.api.service;
 
 import org.jugistanbul.secondopinion.api.config.BaseMockitoTest;
-import org.jugistanbul.secondopinion.api.dto.PatientRequest;
+import org.jugistanbul.secondopinion.api.dto.PatientInformation;
 import org.jugistanbul.secondopinion.api.dto.PatientResponse;
+import org.jugistanbul.secondopinion.api.entity.Patient;
 import org.jugistanbul.secondopinion.api.exception.EntityValidationException;
+import org.jugistanbul.secondopinion.api.repository.PatientRepository;
+import org.jugistanbul.secondopinion.api.service.converter.PatientRequestToEntityConverter;
 import org.jugistanbul.secondopinion.api.service.validator.PatientValidator;
 import org.junit.Test;
+import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 public class PatientServiceTest extends BaseMockitoTest {
 
@@ -21,15 +27,27 @@ public class PatientServiceTest extends BaseMockitoTest {
     @Mock
     PatientValidator patientValidator;
 
+    @Mock(answer = Answers.CALLS_REAL_METHODS)
+    PatientRequestToEntityConverter patientConverter;
+
+    @Mock
+    PatientRepository patientRepository;
+
+
     //Happy path
     @Test
     public void should_create_patient() throws Exception {
         //Given
-        PatientRequest request = new PatientRequest();
+        PatientInformation request = new PatientInformation();
         request.setUsername("eilhan");
         request.setPassword("test123");
         request.setEmail("eilhan@gmail.com");
         request.setPhone("05309547629");
+
+        Patient patient = patientConverter.apply(request);
+
+        //When
+        when(patientRepository.save(any(Patient.class))).thenReturn(patient);
 
         //When
         PatientResponse patientResponse = patientService.create(request);
@@ -42,7 +60,7 @@ public class PatientServiceTest extends BaseMockitoTest {
     @Test
     public void should_throw_exception_when_request_is_missing() throws Exception {
         //Given
-        PatientRequest request = null;
+        PatientInformation request = null;
 
         doThrow(EntityValidationException.class).when(patientValidator).validate(request);
 
