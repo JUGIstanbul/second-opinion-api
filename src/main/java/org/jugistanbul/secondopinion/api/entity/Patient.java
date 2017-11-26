3a2189f546fc61357b618895bc3e7da9ee31c351
Erdem Ilhan <eilhan@deloitte.com>
2017-11-26 08:55:35 +0300

    signUp feature Patient entity extended

--- src/main/java/org/jugistanbul/secondopinion/api/entity/Patient.java
+++ src/main/java/org/jugistanbul/secondopinion/api/entity/Patient.java
@@ -3,20 +3,50 @@ package org.jugistanbul.secondopinion.api.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-/**
- * @author Gökalp Gürbüzer (gokalp.gurbuzer@yandex.com)
- */
+
 @Entity
 @Table(name = "patient")
 public class Patient extends EntityBase {
 
-    private String name;
+    private String userName;
+    
+    private String password;
+    
+    private String email;
+    
+    private String phoneNumber;
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+    
+    
 }
