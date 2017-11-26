ca7ec1cdcc6b08ce3bf6377d80e733be4a2f56ee
zekicelik <celik_zeki@yahoo.com>
2017-11-26 14:54:04 +0300

    Add city, expertise and University controllers' integration tests

---
+++ src/test/java/org/jugistanbul/secondopinion/api/controller/ExpertiseControllerIT.java
@@ -0,0 +1,38 @@
+package org.jugistanbul.secondopinion.api.controller;
+
+import org.jugistanbul.secondopinion.api.config.BaseIT;
+import org.jugistanbul.secondopinion.api.entity.Expertise;
+import org.jugistanbul.secondopinion.api.repository.ExpertiseRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ExpertiseControllerIT extends BaseIT {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    ExpertiseRepository expertiseRepository;
+
+    @Test
+    public void should_list_all_expertise() {
+        //given
+        Expertise expertise = new Expertise();
+        expertise.setName("Oncology");
+        expertiseRepository.save(expertise);
+
+        //when
+        ResponseEntity<List> entity = testRestTemplate.withBasicAuth("1", "1").getForEntity("/v1/expertise", List.class);
+
+        //then
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody().size()).isEqualTo(1);
+    }
+}
