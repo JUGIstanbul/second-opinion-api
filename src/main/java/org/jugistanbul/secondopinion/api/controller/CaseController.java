3553a34664dfc14b1c37228b478d5e29da04a7a8
anil <anillcosar@gmail.com>
2017-11-26 17:45:25 +0300

    Case tests added

--- src/main/java/org/jugistanbul/secondopinion/api/controller/CaseController.java
+++ src/main/java/org/jugistanbul/secondopinion/api/controller/CaseController.java
@@ -1,9 +1,7 @@
 package org.jugistanbul.secondopinion.api.controller;
 
 import org.jugistanbul.secondopinion.api.entity.Case;
-import org.jugistanbul.secondopinion.api.entity.ModelStatus;
 import org.jugistanbul.secondopinion.api.service.CaseService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +15,7 @@ public class CaseController {
         this.caseService = caseService;
     }
 
-    @PostMapping(value="/cases")
+    @PostMapping(value = "/cases")
     @ResponseBody
     public ResponseEntity save(@RequestBody Case aCase) {
         return caseService.createCase(aCase);
@@ -25,20 +23,20 @@ public class CaseController {
 
     @DeleteMapping(value = "/cases/{caseid}")
     @ResponseBody
-    public ResponseEntity delete(@PathVariable("caseid") Long caseId){
+    public ResponseEntity delete(@PathVariable("caseid") Long caseId) {
         return caseService.deleteCase(caseId);
     }
 
     @GetMapping(value = "/cases/{caseid}")
     @ResponseBody
-    public ResponseEntity getCaseById(@PathVariable("caseid") Long caseId){
+    public ResponseEntity getCaseById(@PathVariable("caseid") Long caseId) {
         return caseService.getCaseById(caseId);
     }
 
     @PutMapping(value = "/cases/{caseid}")
     @ResponseBody
-    public ResponseEntity getCaseById(@PathVariable("caseid") Long caseId ,@RequestBody Case aCase){
-        return caseService.putCase(caseId,aCase);
+    public ResponseEntity getCaseById(@PathVariable("caseid") Long caseId, @RequestBody Case aCase) {
+        return caseService.updateCase(caseId, aCase);
     }
 
 }
