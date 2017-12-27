5a41fb1c327e8298c3b7ad60b39df566c6b4c803
Ali Dere <adere@deloitte.com>
2017-12-27 20:17:35 +0300

    bugfix for gender

--- src/main/java/org/jugistanbul/secondopinion/api/service/converter/PatientEntityToInformationConverter.java
+++ src/main/java/org/jugistanbul/secondopinion/api/service/converter/PatientEntityToInformationConverter.java
@@ -15,7 +15,9 @@ public class PatientEntityToInformationConverter implements Converter<Patient, P
     patientInformation.setPassword(patient.getPassword());
     patientInformation.setUsername(patient.getUsername());
     patientInformation.setPhone(patient.getPhone());
-    patientInformation.setGender(patient.getGender().toString());
+    if (patient.getGender() != null) {
+      patientInformation.setGender(patient.getGender().toString());
+    }
     patientInformation.setAddress(patient.getAddress());
     patientInformation.setAddictiveDrugProfile(patient.getAddictiveDrugProfile());
     patientInformation.setAlcoholConsumptionProfile(patient.getAlcoholConsumptionProfile());
